1c065e7fe884d7b1df372743be7138cca4488ec4
engrHarvAbantao <harveyabantao@gmail.com>
2024-10-06 06:40:29 +0800

    for credential json file

--- backend/config/gcsConfig.js
+++ backend/config/gcsConfig.js
@@ -1,14 +1,24 @@
 const { Storage } = require('@google-cloud/storage');
-const path = require('path');
+require('dotenv').config();
 
-// Initialize Google Cloud Storage with the service account key
+// Initialize Google Cloud Storage with the credentials from environment variables
 const storage = new Storage({
-  keyFilename: path.join(__dirname, '../credentials/fitnessforge.json'),  // Path to your JSON key file
-  projectId: 'fitnessforge-437417',  // Replace with your Google Cloud project ID
+  projectId: process.env.GCS_PROJECT_ID,
+  credentials: {
+    type: process.env.GCS_TYPE,
+    private_key_id: process.env.GCS_PRIVATE_KEY_ID,
+    private_key: process.env.GCS_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Ensure newlines are correctly formatted
+    client_email: process.env.GCS_CLIENT_EMAIL,
+    client_id: process.env.GCS_CLIENT_ID,
+    auth_uri: process.env.GCS_AUTH_URI,
+    token_uri: process.env.GCS_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GCS_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GCS_CLIENT_CERT_URL,
+  },
 });
 
 // Specify the bucket name
-const bucketName = 'fitnessforge-image';  // Replace with your bucket name
+const bucketName = 'fitnessforge-image'; // Replace with your bucket name
 const bucket = storage.bucket(bucketName);
 
 module.exports = bucket;
